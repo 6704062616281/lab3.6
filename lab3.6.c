f323760609beacf9e300cf0eff4db65c9778c959
6704062616281 <s6704062616281@email.kmutnb.ac.th>
2025-12-15 16:25:12 +0700

    error score

--- lab3.6.c
+++ lab3.6.c
@@ -3,11 +3,13 @@ int main(){
 	int score;
 	scanf("%d",&score);
 	
-	if(score >= 68){
+	if(score >= 100 && score < 0){
+		if(score >= 68){
 		if(score >= 85){
 			printf("A");
 		}else if(score >= 75){
 			printf("B");
+			printf("A is %d score",85-score);
 		}else if(score >= 68){
 			printf("C\n");
 			printf("B is %d score",75-score);
@@ -15,9 +17,14 @@ int main(){
 	}else{
 		if(score >= 55){
 			printf("D");
+			printf("C is %d score",68-score);
 		}else{
 			printf("F");
+			printf("D is %d score",55-score);
 		}
+	}	
+	}else{
+		printf(" error score");
 	}
 	return 0;
 }

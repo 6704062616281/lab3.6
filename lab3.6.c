30f8f3fc1811fd1df970ff032f8a9fe83107d9b9
6704062616281 <s6704062616281@email.kmutnb.ac.th>
2025-12-15 16:35:11 +0700

    correct error

--- lab3.6.c
+++ lab3.6.c
@@ -3,28 +3,25 @@ int main(){
 	int score;
 	scanf("%d",&score);
 	
-	if(score >= 100 && score < 0){
-		if(score >= 68){
-		if(score >= 85){
+	if(score >= 68){
+		if(score > 100){
+			printf("error score");
+		}else if(score >= 85){
 			printf("A");
 		}else if(score >= 75){
 			printf("B");
-			printf("A is %d score",85-score);
 		}else if(score >= 68){
 			printf("C\n");
 			printf("B is %d score",75-score);
 		}
 	}else{
-		if(score >= 55){
+		if(score < 0){
+			printf("error score");
+		}else if(score >= 55){
 			printf("D");
-			printf("C is %d score",68-score);
 		}else{
 			printf("F");
-			printf("D is %d score",55-score);
 		}
 	}	
-	}else{
-		printf(" error score");
-	}
 	return 0;
 }
